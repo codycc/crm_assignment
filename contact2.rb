db641ae9ee928479181a247fb949bfbdf48dcc27
Cody Condon <codycondon50@gmail.com>
2016-01-19 15:24:43 -0500

    fixed alot of bugs

--- contact2.rb
+++ contact2.rb
@@ -5,55 +5,65 @@ class Contact
   @@contacts = []
   @@id = 1
 
-  def initialize(id, first_name, last_name, options = {})
+  def initialize(id, first_name, last_name, email,note)
     @id = id
     @first_name = first_name
     @last_name = last_name
-    @email = options[:email]
-    @note = options[:note]
+    @email = email
+    @note = note
   end
 
-  def self.create(first_name, last_name, options = {})
-    new_contact = new(@@id, first_name, last_name, options) # => Contact.new(...)
+  def self.create(first_name, last_name, email,note)
+    new_contact = new(@@id, first_name, last_name,email,note) # => Contact.new(...)
     @@contacts << new_contact
     @@id += 1
     new_contact
   end
 
- def self.get_by_firstname(first_name)
-    @@contacts.find { |contact| contact.first_name == first_name }
- end
 
- # def self.get_by_lastname(last_name)
- #   @@contacts.find { |contact| contact.last_name == last_name}
- # end
- #
- # def self.get_by_full_name(first_name,last_name)
- #   @@contacts.find { |contact| (contact.first_name == first_name) && (contact.last_name == last_name)}
- # end
- #
- # def self.get_by_email(email)
- #   @@contacts.find { |contact| contact.email == email}
- # end
- #
- # def self.get_by_note(note)
- #   @@contacts.find { |contact| contact.note == note }
- # end
+def self.search_by_attribute (num,value)
+   case num
+   when "first_name"
+        contact = @@contacts.find_all { |contact| contact.first_name == value}
+    when "last_name"
+        contact = @@contacts.find_all { |contact| contact.last_name == value}
+    when "email"
+         contact = @@contacts.find_all { |contact| contact.email == value}
+    when "note"
+        contact = @@contacts.find_all { |contact| contact.note == value}
+   end
+end
 
- def self.search_by_attribute(value)
-  @@contacts.each do |contact|
-    if contact.first_name == value || contact.last_name == value || contact.email == value || contact.note == value
-      puts "#{contact.id} #{contact.full_name} #{contact.note} #{contact.email}" 
+
+
+  def update(attribute, value)
+    case attribute
+    when "first_name"
+      @first_name = value
+    when "last_name"
+      @last_name = value
+    when "email"
+      @email = value
+    when "note"
+      @note = value
     end
   end
-end
 
   def self.all
     @@contacts
   end
 
-  def self.delete(user_id)
-  @@contacts.delete_if { |contact| contact.id == user_id}
+  def self.delete_all
+  @@contacts = []
+  @@id = 1 # Reset id counter to 1
+end
+
+  def delete
+   Contact.all.delete(self)
+ end
+
+  def self.get(id)
+    @@contacts.find { |contact| contact.id == id }
   end
 
   def self.find(id)
--- crm2.rb
+++ crm2.rb
@@ -3,7 +3,7 @@ require_relative 'contact2.rb'
 class CRM
   def print_main_menu
     puts "1. Add a contact"
-    puts "2. Modify a contact"
+    puts "2. Update contact"
     puts "3. Display all contacts"
     puts "4. Display contact"
     puts "5. Search By Attribute"
@@ -32,44 +32,44 @@ class CRM
         puts "#{contact.id} Full Name:#{contact.full_name} ,Email:(#{contact.email}), Note:(#{contact.note})"
   end
 
-  def modify_existing_contact
-    puts "Which contact would you like to modify? Please enter their ID."
-    contacts_full_name_for_choices
-    user_id = gets.chomp.to_i
-    contact = Contact.find(user_id)
-    puts "Are you sure you would like to modify this contact? (Yes or no)"
-    answer = gets.chomp.downcase
-      if answer == "yes"
-      puts "Would you like to change their;"
-      attributes_menu
-      choice = gets.chomp.to_i
-        case choice
-        when 1
-          puts "Please enter new First name "
-           contact.first_name = gets.chomp.to_s
-           puts"First name has been changed to #{contact.first_name.capitalize}"
-        when 2
-          puts "Please enter new Last name"
-          contact.last_name = gets.chomp.to_s
-            puts"Last name has been changed to #{contact.last_name.capitalize}"
-        when 3
-          puts "Please enter new email"
-          contact.email= gets.chomp.to_s
-            puts"Email has been changed to #{contact.email.capitalize}"
-        when 4
-          puts "Please enter new note "
-          contact.note = gets.chomp.to_s
-          puts"Note has been changed to #{contact.note.capitalize}"
-        end
-      else
-        puts "\"Yes\" was not selected.. Main menu "
-      end
-  end
+  # def modify_existing_contact
+  #   puts "Which contact would you like to modify? Please enter their ID."
+  #   contacts_full_name_for_choices
+  #   user_id = gets.chomp.to_i
+  #   contact = Contact.get(user_id)
+  #   puts "Are you sure you would like to modify this contact? (Yes or no)"
+  #   answer = gets.chomp.downcase
+  #     if answer == "yes"
+  #     puts "Would you like to change their;"
+  #     attributes_menu
+  #     choice = gets.chomp.to_i
+  #       case choice
+  #       when 1
+  #         puts "Please enter new First name "
+  #          contact.first_name = gets.chomp.to_s
+  #          puts"First name has been changed to #{contact.first_name.capitalize}"
+  #       when 2
+  #         puts "Please enter new Last name"
+  #         contact.last_name = gets.chomp.to_s
+  #           puts"Last name has been changed to #{contact.last_name.capitalize}"
+  #       when 3
+  #         puts "Please enter new email"
+  #         contact.email= gets.chomp.to_s
+  #           puts"Email has been changed to #{contact.email.capitalize}"
+  #       when 4
+  #         puts "Please enter new note "
+  #         contact.note = gets.chomp.to_s
+  #         puts"Note has been changed to #{contact.note.capitalize}"
+  #       end
+  #     else
+  #       puts "\"Yes\" was not selected.. Main menu "
+  #     end
+  # end
 
   def choose_option(choice)
     case choice
     when 1 then add_contact
-    when 2 then modify_existing_contact
+    when 2 then update
     when 3 then display_all_contacts
     when 4 then display_contact
     when 5 then search_by_attribute
@@ -93,7 +93,7 @@ class CRM
     email = gets.chomp.to_s.downcase
     puts "Note"
     note = gets.chomp.to_s.downcase
-    new_contact = Contact.create(first_name, last_name, email: email, note: note)
+    new_contact = Contact.create(first_name, last_name, email,note)
     puts "Contact has been added successfully."
   end
 
@@ -122,7 +122,7 @@ class CRM
     puts "What contact would you like to display an attribute for? Please enter by ID. "
     contacts_full_name_for_choices
     user_id = gets.chomp.to_i
-    contact = Contact.find(user_id)
+    contact = Contact.get(user_id)
       puts "Contact found, which attribute would you like to display?"
       attributes_menu
     attribute_id = gets.chomp.to_i
@@ -136,62 +136,58 @@ class CRM
       end
   end
 
-  def search_by_attribute 
-  puts "Please enter a search request: "
-  attribute_selection = gets.chomp.to_s
-  Contact.search_by_attribute(attribute_selection)
+  def search_by_attribute
+  puts "Are you searching by firstname[1], lastname[2], email[3], or note[4] "
+  num = gets.chomp.to_i
+case num
+when 1
+  num = "first_name"
+when 2
+  num = "last_name"
+when 3
+  num = "email"
+when 4
+  num = "note"
 end
+  puts "Value to search for "
+  value = gets.chomp
+  Contact.search_by_attribute(num,value)
+  end
 
-  # def search_by_attribute
-  # puts "Would you like to search by [1]Firstname, [2]Lastname, [3]Full name, [4]Email or [5]note?"
-  # answer = gets.chomp.to_i
-  # case answer
-  # when 1
-  #     puts "Please enter first name of person"
-  #     first_name = gets.chomp.to_s.downcase
-  #     contact = Contact.get_by_firstname(first_name)
-  #     puts "#{contact.id} Full Name:#{contact.full_name} ,Email:(#{contact.email}), Note:(#{contact.note})"
-  #   when 2
-  #     puts "Please enter last name of person"
-  #     last_name = gets.chomp.to_s.downcase
-  #     contact = Contact.get_by_lastname(last_name)
-  #     puts "#{contact.id} Full Name:#{contact.full_name} ,Email:(#{contact.email}), Note:(#{contact.note})"
-  #   when 3
-  #     puts "Please enter first name of person"
-  #     first_name = gets.chomp.to_s.downcase
-  #     puts "Please enter last name of person"
-  #     last_name = gets.chomp.to_s.downcase
-  #     contact = Contact.get_by_full_name(first_name,last_name)
-  #       puts "#{contact.id} Full Name:#{contact.full_name} ,Email:(#{contact.email}), Note:(#{contact.note})"
-  #   when 4
-  #     puts "Please enter the email of desired person"
-  #     email = gets.chomp.downcase
-  #     contact = Contact.get_by_email(email)
-  #       puts "#{contact.id} Full Name:#{contact.full_name} ,Email:(#{contact.email}), Note:(#{contact.note})"
-  #   when 5
-  #     puts "Please enter the note of desired person"
-  #     note = gets.chomp.downcase
-  #     contact = Contact.get_by_note(note)
-  #       puts "#{contact.id} Full Name:#{contact.full_name} ,Email:(#{contact.email}), Note:(#{contact.note})"
-  #   else
-  #     puts "Please enter 1 2 or 3"
-  #   end
-  # end
-
+def update
+    puts "Which contact would you like to modify? Please enter their ID."
+    contacts_full_name_for_choices
+    user_id = gets.chomp.to_i
+    contact = Contact.get(user_id)
+    puts "Would you like to update firstname[1],lastname[2],email[3],or note[4]"
+    attribute = gets.chomp.to_i
+    case attribute
+    when 1 then
+      attribute = "first_name"
+      puts "Please enter new first name! "
+      value =gets.chomp.to_s
+    when 2 then
+      attribute = "last_name"
+      puts "Please enter new last name! "
+      value =gets.chomp.to_s
+    when 3 then
+      attribute = "email"
+      puts "Please enter new email"
+      value =gets.chomp.to_s
+    when 4 then
+      attribute = "note"
+      puts "Please enter new note"
+      value =gets.chomp.to_s
+    end
+    contact.update(attribute,value)
+  end
 
  def delete_contact
-   puts "Which contact would you like to delete? Please enter by ID."
-   contacts_full_name_for_choices
-   user_id = gets.chomp.to_i
-   puts "Are you sure you'd like to delete this contact? Please enter yes or no"
-   confirmation = gets.chomp.downcase
-    if confirmation == "yes"
-          Contact.delete(user_id)
-        puts"Contact deleted, Main Menu.."
-    else
-      puts "\"Yes\" was not selected, aborting... main menu.."
-    end
- end
+    puts "Which contact would you like to delete? Please enter by ID."
+    contacts_full_name_for_choices
+    user_id = gets.chomp.to_i
+    Contact.get(user_id).delete
+  end
  end
 
 crm = CRM.new
